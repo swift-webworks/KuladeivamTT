b49ae280cb103da9441f1c8afebd61b57df1a1b4
swift-webworks <swiftwebworks05@gmail.com>
2026-07-05 16:10:55 +0530

    service pg updated

--- KD/settings.py
+++ KD/settings.py
@@ -13,7 +13,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
-    "localhost"
+    "localhost",
     ".onrender.com",
     "swiftwebworks.pythonanywhere.com",
     "kuladeivamtoursandtravels.onrender.com",
